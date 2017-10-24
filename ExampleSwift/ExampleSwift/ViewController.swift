9141d01f3a1759a5260e638e1ebd1d7c03da395e
David Nedrow <dnedrow@me.com>
2017-10-24 11:23:49 -0400

    Added controls and outlets.

--- ExampleSwift/ExampleSwift/ViewController.swift
+++ ExampleSwift/ExampleSwift/ViewController.swift
@@ -9,7 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var topLabel: UILabel!
+    @IBOutlet weak var button: UIButton!
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var stackView: UIStackView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
